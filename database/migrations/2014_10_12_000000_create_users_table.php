3bb9a4060899934791946557e03858dcd963fa79
hopeconsultants <hopeconsultants786@gmail.com>
2018-07-26 01:36:28 +0500

    Hope Consultant Base Structure as a First Commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('mobile');
+            $table->string('address');
+            $table->string('sex');
+            $table->string('dob');
+            $table->string('bloodgrp');
+            $table->string('photo');
+            $table->string('status');
+            $table->string('is_deleted');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
